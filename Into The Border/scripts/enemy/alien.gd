8f81fdeffcf06c5c551453353a66877172bfa936
Guilherme Torres <guilhermetorres97@gmail.com>
2017-07-25 16:09:49 -0300

    alien bugs fized

--- Into The Border/scripts/enemy/alien.gd
+++ Into The Border/scripts/enemy/alien.gd
@@ -3,6 +3,12 @@ extends KinematicBody2D
 #stats
 var speed = 200
 var hp = 40
+var shot_cooldown = 1
+var last_shot = 0
+
+#player localization
+onready var player = get_node("../").get_children()[0]
+var player_pos = Vector2(0, 0)
 
 #onready
 onready var anim = get_node("anim")
@@ -12,7 +18,7 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
-	pass
+	player_pos = player.get_global_position()
 
 func TakeDamage(value):
 	hp -= value
