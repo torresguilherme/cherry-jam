29e4e251df63bbb592fb66037705ee46a70e1dcc
Guilherme Torres <guilhermetorres97@gmail.com>
2017-07-28 15:13:38 -0300

    start screen OK

--- Into The Border/scripts/global.gd
+++ Into The Border/scripts/global.gd
@@ -9,5 +9,15 @@ var GROUND_GROUP = "ground"
 var E_BULLET_GROUP = "enemy-bullet"
 var P_BULLET_GROUP = "player-bullet"
 
+var scenes = []
+var current_scene = 0
+
 func _ready():
+	scenes.append(load("res://nodes/scene/start-screen.tscn"))
+	scenes.append(load("res://nodes/scene/level1.tscn"))
 	pass
+
+func NextScene():
+	current_scene += 1
+	get_tree().change_scene_to(scenes[current_scene])
+	print(get_tree().get_current_scene())
---
+++ Into The Border/scripts/ui/label.gd
@@ -0,0 +1,13 @@
+extends Label
+
+func _ready():
+	pass
+
+func Text(value):
+	if value == 1:
+		set_text("Jegus9's entry for GDN's Cherry Jam")
+	elif value == 2:
+		set_text("INTO THE BORDER")
+
+func NextScene():
+	global.NextScene()
